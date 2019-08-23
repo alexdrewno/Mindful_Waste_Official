d9dd53c1c4c615a86efa78587e58e05ef492c148
kirbec <lolkirbec@gmail.com>
2019-08-23 10:54:03 -0500

    designed intro screen

---
+++ MindfulWasteOfficial/MindfulWasteOfficial/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  MindfulWasteOfficial
+//
+//  Created by Alex Drewno on 8/23/19.
+//  Copyright © 2019 Alex Drewno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    
+    var MAIN_COLOR = UIColor(red: 252, green: 227, blue: 138, alpha: 1)
+    var OFF_COLOR = UIColor(red: 243, green: 129, blue: 129, alpha: 1)
+    
+}
