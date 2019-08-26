9368003ea5e23578b8e15c4901b5717b30fe35bb
kirbec <lolkirbec@gmail.com>
2019-08-26 18:35:52 -0500

    added cell

---
+++ MindfulWasteOfficial/MindfulWasteOfficial/MainFiles/TableViewCells/NewReportCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewReportCell.swift
+//  MindfulWasteOfficial
+//
+//  Created by Alex Drewno on 8/26/19.
+//  Copyright © 2019 Alex Drewno. All rights reserved.
+//
+
+import Foundation
+import UIKit 
+
+class NewReportCell: UITableViewCell {
+    
+}
