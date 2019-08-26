31022b3657d3e022455e72476a12b9b76402bf9d
kirbec <lolkirbec@gmail.com>
2019-08-26 16:14:58 -0500

    added newReportVC with some flow

---
+++ MindfulWasteOfficial/MindfulWasteOfficial/MainFiles/ViewControllers/NewReportViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NewReportViewController.swift
+//  MindfulWasteOfficial
+//
+//  Created by Alex Drewno on 8/26/19.
+//  Copyright © 2019 Alex Drewno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewReportViewController: MindfulWasteBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func backToHome(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+//MARK: - Actions
+extension NewReportViewController {
+    
+}
