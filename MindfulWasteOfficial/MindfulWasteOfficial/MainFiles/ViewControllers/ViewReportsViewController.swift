cfb4093bdcdd22ac30fe7cac0f57c9471833c00d
kirbec <lolkirbec@gmail.com>
2019-08-27 13:10:54 -0500

    progress....

---
+++ MindfulWasteOfficial/MindfulWasteOfficial/MainFiles/ViewControllers/ViewReportsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewReportsViewController.swift
+//  MindfulWasteOfficial
+//
+//  Created by Alex Drewno on 8/27/19.
+//  Copyright © 2019 Alex Drewno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewReportsViewController: MindfulWasteBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
